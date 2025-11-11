3d503d14403ee0b802c18f5de44264f2e938dec4
Soalagee <soalagee@gmail.com>
2025-11-11 02:19:12 +0100

    Add 0-log_queries.py

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import sqlite3
+import functools
+
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT
+)
+''')
+# Optional: insert some test data
+cursor.execute("INSERT INTO users (name, email) VALUES ('Soala', 'soala@example.com')")
+conn.commit()
+conn.close()
+
+
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper_log_queries(*args, **kwargs):
+        query = kwargs.get("query", args[0] if args else None)
+        if query:
+            print(f"Executing SQL Query: {query}")
+        else:
+            print("No SQL query provided.")
+        return func(*args, **kwargs)
+    return wrapper_log_queries
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
