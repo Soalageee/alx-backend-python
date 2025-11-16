7547c4b1b39b961b2cdf62be652e84b70aab9ef8
Soalagee <soalagee@gmail.com>
2025-11-16 23:32:35 +0100

    Task 4: Add NestedDefaultRouter and api-auth for DRF

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,10 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+
+conversation_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversation_router.register(r'messages', MessageViewSet, basename='conversation-message')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversation_router.urls)),
 ]
