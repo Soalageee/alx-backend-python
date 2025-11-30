1cae509814692cc961f1069934dadfc610e439c9
Soalagee <soalagee@gmail.com>
2025-12-01 00:11:19 +0100

    Add Message pagination and filtering

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -3,4 +3,12 @@ from rest_framework.pagination import PageNumberPagination
 class MessagePagination(PageNumberPagination):
     page_size = 20  # 20 messages per page
     page_size_query_param = 'page_size'  # Allow clients to override page size
-    max_page_size = 50
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,  
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -82,7 +82,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     
 }
